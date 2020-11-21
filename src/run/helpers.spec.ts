20e916c5a59137c01d1614f1cd3e33b207b7735d
George Boozin <boozingeorge@gmail.com>
2020-11-21 15:34:28 +0300

    add test to helpers

--- src/run/helpers.spec.ts
+++ src/run/helpers.spec.ts
@@ -1,9 +1,23 @@
-import { enhanceFilePath } from './helpers';
+import { splitFilesToThreads, enhanceFilePath } from './helpers';
 
 
-describe("helpers", () => {
-    test("enhanceFilePath", () => {
-        const enhancedFiles = enhanceFilePath(['1.js', '2.js'], 'cypress/integration')
+describe('helpers', () => {
+    test('splitFilesToThreads', () => {
+        const threadsWithFilesEven = splitFilesToThreads(['1.js', '2.js'], 2);
+        expect(threadsWithFilesEven).toEqual([['1.js'], ['2.js']]);
+        expect(threadsWithFilesEven).toHaveLength(2);
+
+        const threadsWithFilesOdd = splitFilesToThreads(['1.js', '2.js', '3.js'], 2);
+        expect(threadsWithFilesOdd).toEqual([['1.js', '3.js'], ['2.js']]);
+        expect(threadsWithFilesOdd).toHaveLength(2);
+
+        const threadsWithFilesThree = splitFilesToThreads(['1.js', '2.js', '3.js'], 3);
+        expect(threadsWithFilesThree).toEqual([['1.js'], ['2.js'], ['3.js']]);
+        expect(threadsWithFilesThree).toHaveLength(3);
+    });
+
+    test('enhanceFilePath', () => {
+        const enhancedFiles = enhanceFilePath(['1.js', '2.js'], 'cypress/integration');
         expect(enhancedFiles).toEqual(['cypress/integration/1.js', 'cypress/integration/2.js']);
         expect(enhancedFiles).toHaveLength(2);
     });
